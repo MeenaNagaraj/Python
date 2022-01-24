b1c3014ee5078a862dda30bb907269124990e9ad
MeenaNagaraj <98210066+MeenaNagaraj@users.noreply.github.com>
2022-01-25 01:13:17 +0530

    Add files via upload

---
+++ PerimeterOfCircle.py
@@ -0,0 +1,8 @@
+print("The Perimeter of the Circle")
+
+#Formula is 2*pi*r
+
+pi=3.14
+r=float(input("Enter Radius Value:"))
+perimeter=int(2*pi*r)
+print("The Perimeter of the Circle with Radius ",r," is ",perimeter)        
