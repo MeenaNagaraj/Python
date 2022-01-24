2f6c840c14d256ce903f2caf8d76e13363f03cc4
MeenaNagaraj <98210066+MeenaNagaraj@users.noreply.github.com>
2022-01-25 01:23:41 +0530

    Delete AreaOfCircle.py

--- AreaOfCircle.py
+++
@@ -1,8 +0,0 @@
-print("Area of the Cirle")
-
-#Formula pi*r^2
-
-pi=3.14
-r=float(input("Enter The Radius Value:"))
-area=int(pi*r*r) # we can use r**2 instead of r*r
-print("The area of the circle with radius ",r," is ",area)        
