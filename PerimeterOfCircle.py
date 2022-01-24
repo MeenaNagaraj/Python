0f60a2d0d0f2ad471cedffe9061e3f50ff31f005
MeenaNagaraj <98210066+MeenaNagaraj@users.noreply.github.com>
2022-01-25 01:23:53 +0530

    Delete PerimeterOfCircle.py

--- PerimeterOfCircle.py
+++
@@ -1,8 +0,0 @@
-print("The Perimeter of the Circle")
-
-#Formula is 2*pi*r
-
-pi=3.14
-r=float(input("Enter Radius Value:"))
-perimeter=int(2*pi*r)
-print("The Perimeter of the Circle with Radius ",r," is ",perimeter)        
