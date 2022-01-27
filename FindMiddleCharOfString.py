6d9a115104f0f4c6181a811d428b22c46a0518a1
MeenaNagaraj <98210066+MeenaNagaraj@users.noreply.github.com>
2022-01-28 01:40:53 +0530

    Add files via upload

---
+++ FindMiddleCharOfString.py
@@ -0,0 +1,7 @@
+#Task 1: Find the middle character of the string
+name=input("Enter Your Name: ")
+length=len(name)
+middle_char=length//2
+print("Hi! Your name is {} and the middle letter of your name is {} ".format(name,name[middle_char]))
+
+
