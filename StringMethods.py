4260dde416f77db977805f72b4e7d2596ea4d350
MeenaNagaraj <98210066+MeenaNagaraj@users.noreply.github.com>
2022-01-28 01:28:29 +0530

    Add files via upload

---
+++ StringMethods.py
@@ -0,0 +1,23 @@
+x="Python New"
+
+#center() --> Returns a centered string
+#a=x.center(75) 
+print(x.center(55)) #str.center(length) --->default char is space
+print(x.center(55,"x")) #str.center(length,char)
+
+#rjust()
+print("\n",x.rjust(25,"*"))
+
+#ljust()
+print(x.ljust(25,"*"))
+
+#isalpha()
+s="python3"
+print("\n\nYour String is ",s)
+print("String",s,"is isalpha() -",s.isalpha()) #Returns True if all chars in string are alphabet
+print("String",s,"is isalnum() -",s.isalnum())
+print("String",s,"is islower() -",s.islower())
+
+
+
+
