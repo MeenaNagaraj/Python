a58f41388d54a58ed6d8320e9e5c2f9af999623c
MeenaNagaraj <98210066+MeenaNagaraj@users.noreply.github.com>
2022-01-30 01:37:00 +0530

    Add files via upload

---
+++ Jan23-Task2.py
@@ -0,0 +1,8 @@
+#a = [1,2,3,4,[100,101,102,"Computer_science"],200,203]
+#Extract
+#science
+#computer
+
+a=[1,2,3,4,[100,101,102,"Computer_science"],200,203]
+print(a[4][3][9:])
+print(a[4][3][:8])
