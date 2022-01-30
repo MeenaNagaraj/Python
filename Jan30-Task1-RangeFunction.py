02d7fa4be8212de251f687a52b6dda84bbffb8c8
MeenaNagaraj <98210066+MeenaNagaraj@users.noreply.github.com>
2022-01-30 20:04:41 +0530

    Add files via upload

---
+++ Jan30-Task1-RangeFunction.py
@@ -0,0 +1,23 @@
+#Jan 30 Task1:
+
+#Using Range function  print multiples of 5 from 0 to 75
+print("multiples of 5 from 0 to 75:")
+for i in range(76):
+    if i%5==0:
+        print(i)
+#Using Range function  print multiples of 8 from 0 to 72
+print("multiples of 8 from 0 to 72:")
+for i in range(73):
+    if i%8==0:
+        print(i)
+#Using Range function  print multiples of 5 from 75 to 0
+print("multiples of 5 from 75 to 0:")
+for i in range(75,0,-1):
+    if i%5==0:
+        print(i)
+#Using Range function  print multiples of 8 from 96 to 72
+print("multiples of 8 from 96 to 72:")
+for i in range(96,71,-1):
+    if i%8==0:
+        print(i)
+        
