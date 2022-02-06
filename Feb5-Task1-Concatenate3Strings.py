30aa3e7e6e099f2147b970d53b6f0d6366f6c30d
MeenaNagaraj <98210066+MeenaNagaraj@users.noreply.github.com>
2022-02-06 13:03:44 +0530

    Update Feb5-Task1-Concatenate3Strings.py

--- Feb5-Task1-Concatenate3Strings.py
+++ Feb5-Task1-Concatenate3Strings.py
@@ -1,3 +1,5 @@
+#Write function to concatenate three strings
+
 def str_con(x,y,z):
     return x+" "+y+" "+z
 
@@ -5,3 +7,11 @@ s1=input("Enter String1: ")
 s2=input("Enter String2: ")
 s3=input("Enter String3: ")
 print(str_con(s1,s2,s3))
+
+'''
+Output
+Enter String1: Object
+Enter String2: Oriented
+Enter String3: Programming
+Object Oriented Programming
+'''
