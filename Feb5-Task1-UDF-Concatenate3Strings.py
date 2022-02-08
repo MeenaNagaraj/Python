9593504cf8065e4e4d46bf70fc0d42e01d899eab
MeenaNagaraj <98210066+MeenaNagaraj@users.noreply.github.com>
2022-02-08 23:10:34 +0530

    Add files via upload

---
+++ Feb5-Task1-UDF-Concatenate3Strings.py
@@ -0,0 +1,17 @@
+#Write function to concatenate three strings
+def str_con(x,y,z):
+    return x+" "+y+" "+z
+
+s1=input("Enter String1: ")
+s2=input("Enter String2: ")
+s3=input("Enter String3: ")
+print(str_con(s1,s2,s3))
+
+'''
+output
+
+Enter String1: Object
+Enter String2: Oriented
+Enter String3: Programming
+Object Oriented Programming
+'''
