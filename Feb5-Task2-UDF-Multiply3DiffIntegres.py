9c8dd519f202c725edfb2583f2c4a583275d6daa
MeenaNagaraj <98210066+MeenaNagaraj@users.noreply.github.com>
2022-02-08 23:10:45 +0530

    Add files via upload

---
+++ Feb5-Task2-UDF-Multiply3DiffIntegres.py
@@ -0,0 +1,19 @@
+#Write a function multiply three different integers and return a int
+
+def int_multi(x,y,z):
+    return x*y*z
+n1=int(input("Number1: "))
+n2=int(input("Number2: "))
+n3=int(input("Number3: "))
+
+print(int(int_multi(n1,n2,n3)))
+
+'''
+output
+
+Number1: 2
+Number2: 3
+Number3: 4
+24
+
+'''
